1464e96728da5de00e6198b91354cadfe7a00950
DavideGila <davide.gila@gmail.com>
2023-11-13 18:13:39 +0100

    added a select and gave it style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="bg-main">
     <HeaderComponent />
-    <div class="container bg-light">
+    <div class="container py-4">
+      <select class="p-2 w-25 rounded-3" aria-label="Default select example">
+        <option selected>Alien</option>
+      </select>
+    </div>
+    
+    <div class="container bg-light padding-found-cards">
       <h6 class="m-0 text-light bg-dark p-4 fw-bold">Found {{ store.cardList.length }} cards</h6>
     </div>
     <main class="container">
-      <div class="row">
+      <div class="row bg-light padding-cards">
         <Card
         v-for="(element, index) in store.cardList"
         :cardImg="element.card_images[0].image_url_small"
@@ -52,5 +58,10 @@
   .bg-main{
     background-color: #D48F38;
   }
-
+  .padding-found-cards {
+    padding: 60px 60px 0 60px;
+  }
+  .padding-cards {
+    padding: 0 48px 0 48px;
+  }
 </style>
