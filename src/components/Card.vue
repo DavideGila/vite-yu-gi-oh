d3deab75ac99df2022be12d0289999f53cabc5b3
DavideGila <davide.gila@gmail.com>
2023-11-13 16:36:48 +0100

    created card components to make appear all the cards

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="col-2 py-5">
+        <div>
+            <img :src="cardImg" :alt="cardName" class="w-100"/>
+            <div>
+                <h6 class="title">{{ cardName }}</h6>
+                <p>{{ cardArchetype }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { store } from '../data/store'
+
+    export default {
+        name: 'Card',
+        props: {
+            cardImg: String,
+            cardName: String,
+            cardArchetype: String
+        },
+        data() {
+            return {
+                store
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
