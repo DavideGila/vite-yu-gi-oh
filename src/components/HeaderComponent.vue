901e3ffa42c5bea513ab584e87456c1486844b8e
DavideGila <davide.gila@gmail.com>
2023-11-13 17:54:37 +0100

    created HeaderComponent and gave style

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-light d-flex py-3">
+        <img class="object-fit-contain mx-3" src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="">
+        <h1>Yu-Gi-Oh Api</h1>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss">
+    img {
+        width: 50px;
+    }
+</style>
