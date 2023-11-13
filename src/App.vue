2f4ceb74f42091029a8d135ed9a43c6e679ae871
DavideGila <davide.gila@gmail.com>
2023-11-13 17:45:45 +0100

    gave style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <div class="bg-main">
     <!-- <HeaderComponent title="Yu-Gi-Oh Api"/> -->
+    <div class="container bg-light">
+      <h6 class="m-0 text-light bg-dark p-4 fw-bold">Found {{ store.cardList.length }} cards</h6>
+    </div>
     <main class="container">
       <div class="row">
         <Card
@@ -43,6 +46,9 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+  .bg-main{
+    background-color: #D48F38;
+  }
 
 </style>
