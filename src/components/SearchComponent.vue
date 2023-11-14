e55d64acb9af8b00a9ce410817fdaae13cedcac9
DavideGila <davide.gila@gmail.com>
2023-11-14 17:45:31 +0100

    imported axios and store and created a new method to make appear dynamically all the archetypes in the select

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -2,23 +2,35 @@
     <div class="container py-4">
       <select class="p-2 w-25 rounded-3" aria-label="Default select example" v-model="filterStatus" @change="$emit('search', filterStatus)">
         <option value="">All</option>
-        <option value="alien">Alien</option>
-        <option value="infernoble arms">Infernoble Arms</option>
-        <option value="noble knight">Noble Knight</option>
-        <option value="melodious">Melodious</option>
-        <option value="archfiend">Archfiend</option>
+        <option :value="element.archetype_name" v-for="element in store.archetypesList">{{ element.archetype_name }}</option>
       </select>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from '../data/store';
+
     export default {
         name:'SearchComponent',
         data() {
             return {
-                filterStatus: ''
+                filterStatus: '',
+                store
             }
         },
+        methods: {
+            getArchetypes(){
+                const url = store.apiArchetypes;
+                axios.get(url).then((response) =>{
+                    console.log(response.data);
+                    store.archetypesList = response.data;
+                })
+            }
+        },
+        created(){
+            this.getArchetypes()
+        }
     }
 </script>
 
