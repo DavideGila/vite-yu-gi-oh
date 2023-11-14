844ca1b73c23f978d2d16a8c3c4b74af28c0a730
DavideGila <davide.gila@gmail.com>
2023-11-14 14:56:58 +0100

    created SearchComponent and added a select to it

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container py-4">
+      <select class="p-2 w-25 rounded-3" aria-label="Default select example">
+        <option value="alien">Alien</option>
+        <option value="infernoble-arms">Infernoble Arms</option>
+        <option value="noble-knight">Noble Knight</option>
+        <option value="melodious">Melodious</option>
+        <option value="archfiend">Archfiend</option>
+      </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss">
+
+</style>
