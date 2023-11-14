a0624ef7d6324211425731618973cba8baeb2115
DavideGila <davide.gila@gmail.com>
2023-11-14 17:02:57 +0100

    fixed name values

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -3,8 +3,8 @@
       <select class="p-2 w-25 rounded-3" aria-label="Default select example" v-model="filterStatus" @change="$emit('search', filterStatus)">
         <option value="">All</option>
         <option value="alien">Alien</option>
-        <option value="infernoble-arms">Infernoble Arms</option>
-        <option value="noble-knight">Noble Knight</option>
+        <option value="infernoble arms">Infernoble Arms</option>
+        <option value="noble knight">Noble Knight</option>
         <option value="melodious">Melodious</option>
         <option value="archfiend">Archfiend</option>
       </select>
